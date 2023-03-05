dc563d856a01e78347d85fb8ae21b2cb777d0a5d
Jonathan Jacobson <JJacobson@nescot.ac.uk>
2023-03-05 08:34:34 +0000

    24 March 2023 - added controllers StudentController.cs Students.cs WebApiConfig.cs

---
+++ API TEST Porject/Controllers/StudentController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using API_TEST_Porject.Models;
+
+namespace API_TEST_Porject.Controllers
+{
+    public class StudentController : Controller
+    {
+        //================================================================
+        IList<Students> Students = new List<Students>() /* list of all students edit the URL as https://localhost:44329/api/student
+                                                         * To fetch the details of a single student, Edit the URL as: https://localhost:44329/api/student/2 */
+        {
+            new Students()
+                {
+                    StudentId = 1, StudentName = "Mukesh Kumar", Address = "New Delhi", Course = "IT"
+                },
+                new Students()
+                {
+                    StudentId = 2, StudentName = "Banky Chamber", Address = "London", Course = "HR"
+                },
+                new Students()
+                {
+                    StudentId = 3, StudentName = "Rahul Rathor", Address = "Laxmi Nagar", Course = "IT"
+                },
+                new Students()
+                {
+                    StudentId = 4, StudentName = "YaduVeer Singh", Address = "Goa", Course = "Sales"
+                },
+                new Students()
+                {
+                    StudentId = 5, StudentName = "Manish Sharma", Address = "New Delhi", Course = "HR"
+                },
+        };
+        public IList<Students> GetAllStudents()
+        {
+            //Return list of all employees    
+            return Students;
+        }
+        public Students GetStudentDetails(int id)
+        {
+            //Return a single employee detail    
+            var Student = Students.FirstOrDefault(e => e.StudentId == id);
+            if (Student == null)
+            {
+                //throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.NotFound));
+            }
+            return Student;
+        }
+
+        //================================================================
+
+
+
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Student/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Student/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Student/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Student/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Student/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Student/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Student/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
